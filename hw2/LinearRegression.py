902271b4d6d9d50b21a126bd9a68722523992421
Elvis-Pan <topanyan@sina.com>
2020-10-07 09:32:19 +0800

    changed standardize of testing data to use mean and std of training data

--- hw2/LinearRegression.py
+++ hw2/LinearRegression.py
@@ -26,15 +26,7 @@ def read_data(filename):
                 data_line.append(0.0)
             data.append(data_line)
         f.close()
-    return standardize(data)
-
-
-def standardize(data):
-    data_arr = np.array(data)
-    data_res = []
-    for line in data_arr.T:
-        data_res.append((line - np.mean(line)) / np.std(line))
-    return np.array(data_res).T
+    return data
 
 
 def split_data(data):
@@ -51,6 +43,20 @@ class LinearRegression:
         self.b = 0.0
         self.reg_type = reg_type
         self.lambda_ = reg
+        self.X = None
+        self.y = None
+        self.mean = None
+        self.std = None
+
+    def find_params(self, X):
+        self.mean = np.array([np.mean(line) for line in X.T])
+        self.std = np.array([np.std(line) for line in X.T])
+
+    def standardize(self, X):
+        res = []
+        for i in range(X.T.shape[0]):
+            res.append((X.T[i] - self.mean[i]) / self.std[i])
+        return np.array(res).T
 
     def reg(self):
         if self.reg_type is None:
@@ -61,11 +67,14 @@ class LinearRegression:
             return self.lambda_ * np.abs(self.w)
 
     def fit(self, X, y, epoch=1):
+        self.find_params(X)
+        self.X = self.standardize(X)
+        self.y = y
         for e in range(epoch):
-            for i in range(X.shape[0]):
-                y_pred = np.dot(self.w, X[i]) + self.b
-                self.w = self.w - 2 * self.eta * (y_pred - y[i]) * X[i] + self.reg()
-                self.b = self.b - 2 * self.eta * (y_pred - y[i])
+            for i in range(self.X.shape[0]):
+                y_pred = np.dot(self.w, self.X[i]) + self.b
+                self.w = self.w - 2 * self.eta * (y_pred - self.y[i]) * self.X[i] + self.reg()
+                self.b = self.b - 2 * self.eta * (y_pred - self.y[i])
                 print(self.w)
                 print(self.b)
         return self.w, self.b
