2b333285a57cedc2943da0b84f6b8a3cee41c4b1
Elvis-Pan <topanyan@sina.com>
2020-10-22 00:16:05 +0800

    Fix the matrix multiplication in predict, run 100 epochs and save model in Model.pkl

--- hw3/ANN.py
+++ hw3/ANN.py
@@ -3,8 +3,11 @@ Artificial Neural Network
 Author: Elvis Pan
 Email: ypan2@andrew.cmu.edu
 """
+from numba import jit, cuda
 import csv
 import numpy as np
+import time
+import pickle
 import matplotlib.pyplot as plt
 
 _EXP = 0  # large number to avoid exponentiation overflow
@@ -34,7 +37,9 @@ def read_params(filename):
     return np.array(param)
 
 
-alpha_bias_new = read_params("check/updated_beta1.txt")
+def save_model(Model, filename):
+    with open(filename, "wb") as f:
+        pickle.dump(Model, f, pickle.HIGHEST_PROTOCOL)
 
 
 class ANN:
@@ -85,10 +90,10 @@ class ANN:
         self.beta -= self.learning_rate * beta_grad
         self.beta_bias -= self.learning_rate * b_grad
         self.alpha -= self.learning_rate * alpha_grad
-        # print((alpha_bias_new - self.alpha_bias) / a_grad)
         self.alpha_bias -= self.learning_rate * a_grad
 
-    def train(self, train_x, train_y, epoch=1):
+    # @jit(target="cuda")
+    def fit(self, train_x, train_y, epoch=1):
         for e in range(epoch):
             loss_total = 0
             for i in range(len(train_x)):
@@ -102,19 +107,39 @@ class ANN:
     def predict(self, test_x):
         res = []
         for x in test_x:
-            a = self.alpha_bias + np.matmul(self.alpha, x)
+            x_mat = x.reshape((self.M, 1))
+            a = self.alpha_bias + np.matmul(self.alpha, x_mat)
             z = self.sigmoid(a)
             b = self.beta_bias + np.matmul(self.beta, z)
             y_pred = self.softmax(b)
-            res.append(y_pred)
+            res.append([x[0] for x in y_pred])
+        return np.array(res)
+
+    @staticmethod
+    def compare(y, y_pred):
+        cnt = 0
+        y1 = np.argmax(y, axis=1)
+        y2 = np.argmax(y_pred, axis=1)
+        for i in range(len(y)):
+            if y1[i] == y2[i]:
+                cnt += 1
+        return cnt / len(y)
 
 
 if __name__ == "__main__":
     train_x, train_y = read_data("data/train.csv")
+    test_x, test_y = read_data("data/test.csv")
     Model = ANN(784, 256, 10, learning_rate=0.01)
     Model.alpha = read_params("params/alpha1.txt")
     Model.alpha_bias = read_params("params/beta1.txt")
     Model.beta = read_params("params/alpha2.txt")
     Model.beta_bias = read_params("params/beta2.txt")
-    Model.train(train_x, train_y, epoch=1)
-    print(Model.loss)
+    start_time = time.time()
+    Model.fit(train_x, train_y, epoch=100)
+    train_y_pred = Model.predict(train_x)
+    print("Training finished " + str(time.time() - start_time))
+    save_model(Model, "model.pkl")
+    print(Model.compare(train_y, train_y_pred))
+    test_y_pred = Model.predict(test_x)
+    print(Model.compare(test_y, test_y_pred))
+    print("Finished" + str(time.time() - start_time))
