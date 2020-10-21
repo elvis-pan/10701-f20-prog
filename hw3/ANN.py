08ecf9f002e10a133f7a7863c957e828525735d5
Elvis-Pan <topanyan@sina.com>
2020-10-21 18:46:10 +0800

    Implement forward functions

--- hw3/ANN.py
+++ hw3/ANN.py
@@ -6,44 +6,51 @@ Email: ypan2@andrew.cmu.edu
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 class ANN:
-    def __init__(self, M, D, K):
-        self.M = M
-        self.D = D
-        self.K = K
-        self.N = None
-        self.x = None
-        self.y = None
-        self.y_pred = None
-        self.alpha = np.zeros((D, M+1))
-        self.beta = np.zeros((K, D+1))
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        self.M = input_dim
+        self.D = hidden_dim
+        self.K = output_dim
+        self.alpha = np.zeros((self.D, self.M))
+        self.alpha_bias = np.zeros(self.D)
+        self.beta = np.zeros((self.K, self.D))
+        self.beta_bias = np.zeros(self.K)
         pass
 
-    def linear_forward(self, x, weight, bias):
-        pass
+    @staticmethod
+    def linear_forward(x, weight, bias):
+        return bias + np.dot(weight, x)
 
     def linear_backward(self):
         pass
 
-    def sigmoid_forward(self, x):
+    @staticmethod
+    def sigmoid_forward(x):
         return 1 / (1 + np.exp(-x))
 
     def sigmoid_backward(self):
         pass
 
-    def softmax_xeloss_forward(self, x, labels):
-        return np.exp(x) / np.sum(np.exp(x))
+    @staticmethod
+    def softmax_xeloss_forward(x, labels):
+        pred = np.exp(x) / np.sum(np.exp(x))
+        return -np.sum(labels * np.log(pred))
 
     def softmax_xeloss_backward(self):
         pass
 
-    def loss(self):
-        return -1 / self.N * np.sum(self.y * np.log(self.y_pred))
+    def forward(self, x):
+        out = self.linear_forward(x, self.alpha, self.alpha_bias)
+        out = self.sigmoid_forward(out)
+        out = self.linear_forward(out, self.beta, self.beta_bias)
+        out = self.softmax_xeloss_forward(out)
+        return out
+
+    def backward(self):
+        pass
 
     def train(self, x, y, epoch=1):
-        self.N = len(x)
-        self.x = x
-        self.y = y
         for e in range(epoch):
             pass
         pass
