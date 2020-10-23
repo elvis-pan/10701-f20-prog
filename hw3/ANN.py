Fix the problem of 0 z gradient due to large weight

hw3/ANN.py
@@ -199,22 +199,30 @@ if __name__ == "__main__":
     # print(Model.accuracy_list)  # Q5.6
     Model.plot_loss(15, train_x, train_y, test_x, test_y, "result/15.png")
     print("15 epoch finished")
+    
+    100 epochs
+    Model = new_model()
+    Model.plot_loss(100, train_x, train_y, test_x, test_y, "result/100.png")
+
+    Model = ANN(784, 256, 10, learning_rate=0.01)
+    Model.alpha = np.zeros((Model.D, Model.M))
+    Model.alpha_bias = np.zeros((Model.D, 1))
+    Model.beta = np.zeros((Model.K, Model.D))
+    Model.beta_bias = np.zeros((Model.K, 1))
+    Model.plot_loss(100, train_x, train_y, test_x, test_y, "result/zero.png")
     """
-    # 100 epochs
-    # Model = new_model()
-    # Model.plot_loss(100, train_x, train_y, test_x, test_y, "result/100.png")
-
-    Model = ANN(784, 100, 10, learning_rate=0.01)
-    Model.plot_loss(100, train_x, train_y, test_x, test_y, "result/D100.png")
-
-    Model = ANN(784, 512, 10, learning_rate=0.01)
-    Model.plot_loss(100, train_x, train_y, test_x, test_y, "result/D512.png")
+    Model = ANN(784, 256, 10, learning_rate=0.01)
+    Model.alpha = np.random.uniform(0, 1, (Model.D, Model.M)) / (Model.D * (Model.M + 1))
+    Model.alpha_bias = np.random.uniform(0, 1, (Model.D, 1)) / (Model.D * (Model.M + 1))
+    Model.beta = np.random.uniform(0, 1, (Model.K, Model.D)) / (Model.K * (Model.D + 1))
+    Model.beta_bias = np.random.uniform(0, 1, (Model.K, 1)) / (Model.K * (Model.D + 1))
+    Model.plot_loss(100, train_x, train_y, test_x, test_y, "result/uniform.png")
 
     """
     Model = ANN(784, 256, 10, learning_rate=0.01)
-    Model.alpha = np.random.uniform(-1, 1, (Model.D, Model.M))
-    Model.alpha_bias = np.random.uniform(-1, 1, (Model.D, 1))
-    Model.beta = np.random.uniform(-1, 1, (Model.K, Model.D))
-    Model.beta_bias = np.random.uniform(-1, 1, (Model.K, 1))
-    Model.plot_loss(100, train_x, train_y, test_x, test_y, "result/uniform.png")
+    Model.alpha = np.random.standard_normal((Model.D, Model.M))
+    Model.alpha_bias = np.random.standard_normal((Model.D, 1))
+    Model.beta = np.random.standard_normal((Model.K, Model.D))
+    Model.beta_bias = np.random.standard_normal((Model.K, 1))
+    Model.plot_loss(100, train_x, train_y, test_x, test_y, "result/normal.png")
     """