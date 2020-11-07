1a62fb44eae1a0e112a1071d010280a9759bf788
Elvis-Pan <topanyan@sina.com>
2020-11-08 04:03:18 +0800

    Implement AdaBoost, need to fix DecisionStump

--- hw4/AdaBoost.py
+++ hw4/AdaBoost.py
@@ -17,11 +17,49 @@ def read_data(filename):
 
 
 class AdaBoost:
-    pass
+    def __init__(self):
+        self.iteration = 0
+        self.learners = []
+        self.weighted_error = []
+        self.votes = []
+        self.weights = None
+        self.y_pred = None
+        self.accuracy = 0
+
+    def fit(self, train_x, train_y, iteration=1):
+        self.iteration = iteration
+        self.weights = np.ones(train_y.shape[0])
+        for k in range(iteration):
+            model = DecisionStump()
+            model.fit(train_x, train_y)
+            train_y_pred = model.predict(train_x)
+            err = np.sum(self.weights * np.abs(train_y - train_y_pred) / 2) / np.sum(self.weights)
+            vote = np.log((1 - err) / err) / 2
+            self.weights = self.weights * np.exp(-train_y * vote * train_y_pred)  # update weights
+            self.votes.append(vote)
+            self.learners.append(model)
+
+    def predict(self, test_x):
+        s = np.zeros(test_x.shape[0])
+        for k in range(self.iteration):
+            s += self.votes[k] * self.learners[k].predict(test_x)
+        self.y_pred = np.array(list(map(lambda x: -1 if x < 0 else 1, s / sum(self.votes))))
+        return self.y_pred
+
+    def eval_model(self, y, y_pred):
+        self.accuracy = np.sum(np.abs(y + y_pred)) / (2 * y.shape[0])
+        return self.accuracy
 
 
 if __name__ == "__main__":
     train_x, train_y = read_data("datasets/train_adaboost.csv")
-    DS = DecisionStump()
-    DS.fit(train_x, train_y)
-    print(DS.accuracy)
+    test_x, test_y = read_data("datasets/test_adaboost.csv")
+    # DS = DecisionStump()
+    # DS.fit(train_x, train_y)
+    # print(DS.accuracy)
+
+    AB = AdaBoost()
+    AB.fit(train_x, train_y, 100)
+    train_y_pred = AB.predict(train_x)
+    AB.eval_model(train_y, train_y_pred)
+    print(AB.accuracy)
--- hw4/DecisionStump.py
+++ hw4/DecisionStump.py
@@ -8,6 +8,9 @@ class DecisionStump:
 
     def __init__(self):
         self.f = None
+        self.direction = None
+        self.boundary = 0
+        self.y_pred = None
         self.accuracy = 0
 
     def fit(self, train_x, train_y):
@@ -15,31 +18,44 @@ class DecisionStump:
         v = [(x[1], y) for (x, y) in zip(train_x, train_y)]
         # Compute horizontal boundary
         lowest_err = len(train_y)
-        for i in range(len(h)):
-            f1 = lambda x: (x[0] < bound and x[1] > 0) or (x[0] > bound and x[1] < 0)
-            f2 = lambda x: (x[0] < bound and x[1] < 0) or (x[0] > bound and x[1] > 0)
+        for i in range(len(h) + 1):
+            f1 = lambda x: (x[0] < bound and x[1] > 0) or (x[0] >= bound and x[1] < 0)
+            f2 = lambda x: (x[0] < bound and x[1] < 0) or (x[0] >= bound and x[1] > 0)
             # Horizontal
-            bound = h[i][0]
+            bound = h[i][0] if i < len(h) else float("inf")
             err1 = len(list(filter(f1, h)))
             err2 = len(list(filter(f2, h)))
             if err1 < lowest_err:
                 lowest_err = err1
+                self.boundary = bound
+                self.direction = "horizontal -1"  # horizontal axis < bound is classified as -1
                 self.f = lambda x: -1 if x[0] < bound else 1
             if err2 < lowest_err:
                 lowest_err = err2
+                self.boundary = bound
+                self.direction = "horizontal 1"
                 self.f = lambda x: 1 if x[0] < bound else -1
             # Vertical
-            bound = v[i][0]
+            bound = v[i][0] if i < len(v) else float("inf")
             err1 = len(list(filter(f1, v)))
             err2 = len(list(filter(f2, v)))
             if err1 < lowest_err:
                 lowest_err = err1
+                self.boundary = bound
+                self.direction = "vertical -1"
                 self.f = lambda x: -1 if x[1] < bound else 1
             if err2 < lowest_err:
                 lowest_err = err2
+                self.direction = "vertical 1"
+                self.boundary = bound
                 self.f = lambda x: 1 if x[1] < bound else -1
         self.accuracy = lowest_err / len(train_y)
         return self.f
 
-    def predict(test_x, test_y):
-        pass
+    def predict(self, test_x):
+        self.y_pred = np.array(list(map(self.f, test_x)))
+        return self.y_pred
+
+    def compute_error(self, y, y_pred):
+        self.accuracy = np.sum(np.abs(y + y_pred)) / (2 * y.shape[0])
+        return self.accuracy
